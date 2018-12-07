9677096a73ec45352f88d776d3a2ef1e1f9dfef5
Gustavo Marques <gutooh@gmail.com>
2018-12-07 15:18:29 +1100

    Success Stories done, Footer and header update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 
 
+// ------------------------------------------------
+// General Scripts
+// ------------------------------------------------
+
 if (screen.width < 1030) {
 	$("body > section").waypoint(function() {
 		$(this).toggleClass("active");
@@ -51,9 +55,85 @@ $btnMobile.click(function(){
 })
 
 
+// ------------------------------------------------
+// Home page carrousel
+// ------------------------------------------------
+
+function moveToSelected(element) {
+
+	if (element == "next") {
+		var selected = $(".selected").next();
+	} else if (element == "prev") {
+		var selected = $(".selected").prev();
+	} else {
+		var selected = element;
+	}
+
+	var next = $(selected).next();
+	var prev = $(selected).prev();
+	var prevSecond = $(prev).prev();
+	var nextSecond = $(next).next();
+
+	$(selected).removeClass().addClass("selected");
+
+	$(prev).removeClass().addClass("prev");
+	$(next).removeClass().addClass("next");
+
+	$(nextSecond).removeClass().addClass("nextRightSecond");
+	$(prevSecond).removeClass().addClass("prevLeftSecond");
+
+	$(nextSecond).nextAll().removeClass().addClass('hideRight');
+	$(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
+}
+
+// Keyboard events
+$(document).keydown(function(e) {
+	switch(e.which) {
+		case 37: // left
+		moveToSelected('prev');
+		break;
+
+		case 39: // right
+		moveToSelected('next');
+		break;
+
+		default: return;
+	}
+	e.preventDefault();
+});
+
+
+$('#shortcutsLinks > main > div').click(function() {
+	moveToSelected($(this));
+});
+
+$('#shortcutsLinks #prev').click(function() {
+	moveToSelected('prev');
+});
+
+$('#shortcutsLinks #next').click(function() {
+	moveToSelected('next');
+});
 
 
 
+
+// ------------------------------------------------
+// Testimonials carrousel
+// ------------------------------------------------
+
+$('#testimonialsWrapper > section').slick({
+	dots: false,
+	infinite: true,
+	speed: 500,
+	slidesToShow: 1,
+	fade: true,
+	cssEase: 'linear',
+	prevArrow: '#testimonialsNav #prev',
+	nextArrow: '#testimonialsNav #next',
+	adaptiveHeight: true
+});
 
 
 
