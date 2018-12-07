9f091324bf0aafc1bd7c090c8764c90ce1960a58
Gustavo Marques <gutooh@gmail.com>
2018-12-07 16:43:09 +1100

    Success Stories responsive

--- js/main.js
+++ js/main.js
@@ -20,28 +20,47 @@ else {
 
 $(document).ready(function() {
 
-	$('#nav > li:has(ul)').addClass('submenu');
-
-	// grab the initial top offset of the navigation 
-	var stickyNavTop = $('#navWrapper').offset().top;
 	
-	// our function that decides weather the navigation bar should have "fixed" css position or not.
-	var stickyNav = function(){
-		var scrollTop = $(window).scrollTop(); // our current vertical position from the top
-			 
-		// if we've scrolled more than the navigation, change its position to fixed to stick to top,
-		// otherwise change it back to relative
-		if (scrollTop > stickyNavTop) { 
-			$('#navWrapper').addClass('sticky');
-		} else {
-			$('#navWrapper').removeClass('sticky'); 
-		}
-	};
+		$('#nav > li:has(ul)').addClass('submenu');
 
-	stickyNav();
-	// and run it again every time you scroll
-	$(window).scroll(function() {
+		// grab the initial top offset of the navigation 
+		var stickyNavTop = $('#navWrapper').offset().top;
+		
+		// our function that decides weather the navigation bar should have "fixed" css position or not.
+		var stickyNav = function(){
+			var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+				 
+			// if we've scrolled more than the navigation, change its position to fixed to stick to top,
+			// otherwise change it back to relative
+			if (scrollTop > stickyNavTop) { 
+				$('#navWrapper').addClass('sticky');
+			} else {
+				$('#navWrapper').removeClass('sticky'); 
+			}
+		};
+
 		stickyNav();
+		// and run it again every time you scroll
+		$(window).scroll(function() {
+			stickyNav();
+		});
+	
+
+
+	// ------------------------------------------------
+	// Testimonials carrousel
+	// ------------------------------------------------
+
+	$('#testimonialsWrapper > section').slick({
+		dots: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		fade: true,
+		cssEase: 'linear',
+		prevArrow: '#testimonialsNav #prev',
+		nextArrow: '#testimonialsNav #next',
+		adaptiveHeight: true
 	});
 
 	
@@ -119,21 +138,7 @@ $('#shortcutsLinks #next').click(function() {
 
 
 
-// ------------------------------------------------
-// Testimonials carrousel
-// ------------------------------------------------
 
-$('#testimonialsWrapper > section').slick({
-	dots: false,
-	infinite: true,
-	speed: 500,
-	slidesToShow: 1,
-	fade: true,
-	cssEase: 'linear',
-	prevArrow: '#testimonialsNav #prev',
-	nextArrow: '#testimonialsNav #next',
-	adaptiveHeight: true
-});
 
 
 
