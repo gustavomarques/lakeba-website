c920d36e60beab9d428e1dae996ed087b7955668
Gustavo Marques <gutooh@gmail.com>
2018-12-18 16:28:35 +1100

    Our approach new image

--- js/main.js
+++ js/main.js
@@ -59,26 +59,26 @@ $(document).ready(function() {
 
 // grab the initial top offset of the navigation 
 
-//	var stickyNavTop = $('#navWrapper').offset().top;
-//	
-//	// our function that decides weather the navigation bar should have "fixed" css position or not.
-//	var stickyNav = function(){
-//		var scrollTop = $(window).scrollTop(); // our current vertical position from the top
-//			 
-//		// if we've scrolled more than the navigation, change its position to fixed to stick to top,
-//		// otherwise change it back to relative
-//		if (scrollTop > stickyNavTop) { 
-//			$('#navWrapper').addClass('sticky');
-//		} else {
-//			$('#navWrapper').removeClass('sticky'); 
-//		}
-//	};
-//
-//	stickyNav();
-//	// and run it again every time you scroll
-//	$(window).scroll(function() {
-//		stickyNav();
-//	});
+	var stickyNavTop = $('#navWrapper').offset().top;
+	
+	// our function that decides weather the navigation bar should have "fixed" css position or not.
+	var stickyNav = function(){
+		var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+			 
+		// if we've scrolled more than the navigation, change its position to fixed to stick to top,
+		// otherwise change it back to relative
+		if (scrollTop > stickyNavTop) { 
+			$('#navWrapper').addClass('sticky');
+		} else {
+			$('#navWrapper').removeClass('sticky'); 
+		}
+	};
+
+	stickyNav();
+	// and run it again every time you scroll
+	$(window).scroll(function() {
+		stickyNav();
+	});
 	
 
 
